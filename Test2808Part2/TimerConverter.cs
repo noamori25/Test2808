eedb5278f8236f5164d68df9ba6890c761ef210c
noamori25 <44751354+noamori25@users.noreply.github.com>
2019-08-31 14:33:22 +0300

    Add files via upload

---
+++ Test2808Part2/TimerConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Test2808Part2
+{
+    class TimerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+           if (System.Convert.ToInt32(value) <= 15)
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+            return new SolidColorBrush(Colors.Black);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test2808Part2/ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Threading;
+
+namespace Test2808Part2
+{
+    public class ViewModel : DependencyObject
+    {
+        public Exercise ex { get; set; }
+        public static readonly DependencyProperty TimerLeftProperty =
+            DependencyProperty.Register("TimerLeft", typeof(int), typeof(ViewModel), new PropertyMetadata(30));
+        public DispatcherTimer timer;
+        public ViewModel()
+        {
+            ex = new Exercise() { CorrectAnswer = 50,Optional1 = 55, Optional2 = 65, Optional3 = 10 };
+            timer = new DispatcherTimer(TimeSpan.FromSeconds(1), 
+                DispatcherPriority.Normal,
+                new EventHandler(Callback), Application.Current.Dispatcher);
+            timer.Start();
+        }
+
+        private void Callback(object ignore, EventArgs ex)
+        {
+            TimerLeft = TimerLeft - 1;
+            if (TimerLeft == 0)
+            {
+                timer.Stop();
+            }
+        }
+
+        public int TimerLeft
+        {
+            get { return (int)GetValue(TimerLeftProperty); }
+            set { SetValue(TimerLeftProperty, value); }
+        }
+
+
+
+    }
+}
